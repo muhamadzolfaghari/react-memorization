26ef50e041cd2005574dee84e66cfa4616702ed1
muhamad <muhamad.zolfaghari@gmail.com>
2024-01-25 18:27:05 +0330

    UseCallback and memo page were added

--- src/AppRoutes.tsx
+++
@@ -1,7 +0,0 @@
-import {Route, Routes} from "react-router-dom";
-import Memo from "./pages/Memo";
-
-
-export default function AppRoutes() {
-    return <Route path="memo" element={<Memo />}/>
-}
--- src/pages/Memo.tsx
+++
@@ -1,5 +0,0 @@
-const Memo = () => {
-  return <>memo</>;
-};
-
-export default Memo;
